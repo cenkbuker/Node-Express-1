8f9ffc5764ada7eb07cdef3e46b073de739e3142
Cenk <bukercenk@gmail.com>
2023-01-16 12:50:47 -0600

    Part-3 added

--- broken-app/app.js
+++ broken-app/app.js
@@ -2,17 +2,39 @@ const express = require('express');
 let axios = require('axios');
 var app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.post('/', function(req, res, next) {
   try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
-
-    return res.send(JSON.stringify(out));
-  } catch {
-    next(err);
+    const fetchGithubInfo = async (url) => {
+      console.log(`Fetching ${url}`)
+      const githubInfo = await axios(url)
+      return {
+        name: githubInfo.data.name,
+        bio: githubInfo.data.bio
+      }
+    }
+    
+    const fetchUserInfo = async (names) => {
+      const requests = names.map((name) => {
+        const url = `https://api.github.com/users/${name}`
+        return fetchGithubInfo(url)
+         .then((a) => {
+          return a 
+          })
+      })
+      return Promise.all(requests) 
+    }
+    
+    
+    return fetchUserInfo(req.body.developers)
+     .then(a => res.send(JSON.stringify(a)))
+    
+  } catch (err){
+    return next(err);
   }
 });
 
-app.listen(3000);
+app.listen(3000, function() {
+  console.log(`Server starting on port 3000`);
+});
